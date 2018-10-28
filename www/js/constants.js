e58954ac68540bac53b832114355c93c1db6dd2a
Cory Blissitte <cory.blissitte@gmail.com>
2018-10-27 23:40:29 -1000

    Accordion Header Fixes

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,5 +1,5 @@
 falloutApp.constant("settings", {
-    apiUrl: "http://localhost:50503",//apiUrl: "https://falloutmaps.azurewebsites.net",//
+    apiUrl: "https://falloutmaps.azurewebsites.net",//apiUrl: "http://localhost:50503",//
     markerUrl: "http://www.falloutmaps.com/markers/",
     updateEndpoint: "/api/update/",
     mapPointsEndpoint: "/api/falloutPoints/",
